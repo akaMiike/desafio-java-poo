dd634bc25f8cf9717a7cb16cda9d5cd49be4815a
akaMiike <mike.nss@alu.ufc.br>
2023-03-08 17:05:19 -0300

    Adicionada classes de dominio de Curso e Mentoria

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+
+    public static void main(String[] args){
+        Curso curso = new Curso();
+        Mentoria mentoria = new Mentoria();
+        System.out.println("Deu certo");
+    }
+}
